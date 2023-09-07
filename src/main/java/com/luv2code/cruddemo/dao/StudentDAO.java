13a489e441d12c1ea2e92da71650c4d9b1ff2fb5
“YRonny <ronnyrit@gmail.com>
2023-09-06 19:35:04 -0700

    first commit

---
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,23 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+
+import java.util.List;
+
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+    Student findById(int id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student student);
+
+    void delete(int id);
+
+    int deleteAll();
+}
