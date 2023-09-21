a33535ef1b87260f08e9f6c9cd7ee5f6504069b5
Ronny <ronnyrit@gmail.com>
2023-09-20 21:19:42 -0700

    Add "add employee" button and submission form, works successfully

---
+++ src/main/java/com/luv2code/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.luv2code.cruddemo.service;
+
+import java.util.List;
+
+import com.luv2code.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
