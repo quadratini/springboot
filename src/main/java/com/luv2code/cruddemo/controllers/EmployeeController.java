00e926ca0d3ac4b6bacbeed06b39d4ae21f4d9d8
Ronny <ronnyrit@gmail.com>
2023-09-26 21:11:14 -0700

    add full crud functionality

--- src/main/java/com/luv2code/cruddemo/controllers/EmployeeController.java
+++ src/main/java/com/luv2code/cruddemo/controllers/EmployeeController.java
@@ -4,10 +4,7 @@ import com.luv2code.cruddemo.entity.Employee;
 import com.luv2code.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,6 +49,29 @@ public class EmployeeController {
         employeeService.save(employee);
 
         // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+
+        // get the employee from the service
+        Employee employee = employeeService.findById(id);
+
+        // set employee in the model to prepopulate the form
+        model.addAttribute("employee", employee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+
+        // delete the employee
+        employeeService.deleteById(id);
+
+        // redirect to the /employees/list
         return "redirect:/employees/list";
     }
 }
